8115415e3b34ed7680e0db68b3c6d658529003ea
Lucas Portella <lucasportella@hotmail.com>
2026-07-24 21:15:54 -0300

    feat: Ajustes finais - retirando Expanded dos 3 cards iniciais

--- aula2/lib/main.dart
+++ aula2/lib/main.dart
@@ -51,37 +51,28 @@ class MyApp extends StatelessWidget {
                   ),
                   child: Row(
                     children: [
-                      Expanded(
-                        flex: 1,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.all(12),
-                          child: Icon(Icons.notifications, color: greenColor),
-                        ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(12),
+                        child: Icon(Icons.notifications, color: greenColor),
                       ),
-                      Expanded(
-                        flex: 9,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Notificações',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                              Text(
-                                'Gerencie seus alertas',
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey.shade600,
-                                ),
-                              ),
-                            ],
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Notificações',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
                           ),
-                        ),
+                          Text(
+                            'Gerencie seus alertas',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
@@ -96,24 +87,15 @@ class MyApp extends StatelessWidget {
                   ),
                   child: Row(
                     children: [
-                      Expanded(
-                        flex: 1,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.all(12),
-                          child: Icon(Icons.lock, color: greenColor),
-                        ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(12),
+                        child: Icon(Icons.lock, color: greenColor),
                       ),
-                      Expanded(
-                        flex: 9,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
-                          child: Text(
-                            'Privacidade e segurança',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                            ),
-                          ),
+                      Text(
+                        'Privacidade e segurança',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
                         ),
                       ),
                     ],
@@ -128,24 +110,15 @@ class MyApp extends StatelessWidget {
                   ),
                   child: Row(
                     children: [
-                      Expanded(
-                        flex: 1,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.all(12),
-                          child: Icon(Icons.help, color: greenColor),
-                        ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.all(12),
+                        child: Icon(Icons.help, color: greenColor),
                       ),
-                      Expanded(
-                        flex: 9,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
-                          child: Text(
-                            'Ajuda e suporte',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                            ),
-                          ),
+                      Text(
+                        'Ajuda e suporte',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
                         ),
                       ),
                     ],
@@ -211,7 +184,7 @@ class MyApp extends StatelessWidget {
                               margin: EdgeInsets.only(right: 12),
                               padding: EdgeInsets.all(16),
                               decoration: BoxDecoration(
-                                color: Colors.grey.shade50,
+                                color: Colors.grey.shade100,
                                 borderRadius: BorderRadius.all(
                                   Radius.circular(12),
                                 ),
@@ -242,7 +215,7 @@ class MyApp extends StatelessWidget {
                             child: Container(
                               padding: EdgeInsets.all(16),
                               decoration: BoxDecoration(
-                                color: Colors.grey.shade50,
+                                color: Colors.grey.shade100,
                                 borderRadius: BorderRadius.all(
                                   Radius.circular(12),
                                 ),
